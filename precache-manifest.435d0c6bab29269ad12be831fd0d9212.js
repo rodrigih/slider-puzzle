2a755879cd5bf6d7659614932d813d12e78b752d
Rodrigo Hernandez <rh3rn4nd3z@gmail.com>
2019-07-14 22:04:34 -0700

    Updates

---
+++ precache-manifest.435d0c6bab29269ad12be831fd0d9212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slider-puzzle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "853e729308272b2505ef",
+    "url": "/slider-puzzle/static/js/runtime~main.853e7293.js"
+  },
+  {
+    "revision": "40d0c8814f6f7daafca8",
+    "url": "/slider-puzzle/static/js/main.40d0c881.chunk.js"
+  },
+  {
+    "revision": "5368bc9d527a4abbe7ce",
+    "url": "/slider-puzzle/static/js/2.5368bc9d.chunk.js"
+  },
+  {
+    "revision": "40d0c8814f6f7daafca8",
+    "url": "/slider-puzzle/static/css/main.97d89af9.chunk.css"
+  },
+  {
+    "revision": "d5df8be85363829beae805bfd12c27f8",
+    "url": "/slider-puzzle/index.html"
+  }
+];
